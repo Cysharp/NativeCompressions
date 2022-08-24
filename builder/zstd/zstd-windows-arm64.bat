ee20b31974e7d76c6d4be31afe5fab7bfd58c19c
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-08-24 10:54:10 +0900

    feat: add windows cmake bat

--- builder/zstd/zstd-windows-arm64.bat
+++ builder/zstd/zstd-windows-arm64.bat
@@ -8,17 +8,13 @@ set GIT_ZSTD_VERSION=%ZSTD_VERSION:~0,-1%
 :: 'v1.5.2 ' -> '1.5.2'
 set FILE_ZSTD_VERSION=%ZSTD_VERSION:~1,-1%
 set OS=win
+set ARCH=ARM64
 set PLATFORM=arm64
 if not defined OUTPUT_DIR (set OUTPUT_DIR=src\runtimes\%OS%-%PLATFORM%)
 
 :: build
-set BASEDIR=%cd%
-rmdir /S /Q %BASEDIR%\zstd\build\cmake\build
-mkdir %BASEDIR%\zstd\build\cmake\build
-cd %BASEDIR%/zstd/build/cmake/build
-cmake -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 17 2022" -A ARM64 ..
-cmake --build . --config Release
-cd %BASEDIR%
+call builder\zstd\core\zstd-builder-windows.bat
+if %ERRORLEVEL% == 1 exit /b 1
 
 :: confirm
 dir zstd\build\cmake\build\lib\Release\zstd*
