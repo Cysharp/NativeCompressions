31e4978438360fa23a046e4caaf66fce37878dd0
Yuichiro Mukai <y.iky917@gmail.com>
2023-07-04 19:36:41 +0900

    Revert "check size_t_is_usize(true)"
    This reverts commit 054f6fc9942d2da61698602705b337772853e6c9.

--- src/liblz4/build.rs
+++ src/liblz4/build.rs
@@ -6,7 +6,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .header("../../lz4/lib/lz4hc.c")
         .header("../../lz4/lib/lz4frame.c")
         .header("../../lz4/lib/xxhash.c")
-        //.size_t_is_usize(false)
+        .size_t_is_usize(false)
         .generate()
         .unwrap()
         .write_to_file("src/lz4.rs")
