f604c36e6845375b7654362fd8022ae25c6aeb61
neuecc <ils@neue.cc>
2023-07-12 16:46:26 +0900

    wip stream encode

--- src/NativeCompressions.Lz4/LZ4Encoder.cs
+++ src/NativeCompressions.Lz4/LZ4Encoder.cs
@@ -1,59 +1,59 @@
-﻿using System;
-using System.Buffers;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.IO.Compression;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.InteropServices;
 using static NativeCompressions.Lz4.Lz4NativeMethods;
 
 namespace NativeCompressions.Lz4
 {
     // https://github.com/lz4/lz4/blob/v1.9.4/lib/lz4.h
+    // https://htmlpreview.github.io/?https://github.com/lz4/lz4/blob/master/doc/lz4_manual.html
 
     public unsafe partial struct LZ4Encoder : IDisposable
     {
+        bool disposed;
         LZ4_stream_u* stream;
 
         public LZ4Encoder()
         {
             this.stream = LZ4_createStream();
+            Reset();
         }
 
+        public void Reset()
+        {
+            ThrowIfDisposed();
+            LZ4_resetStream_fast(stream);
+        }
 
-        //public void Compress(ReadOnlySpan<byte> source, Span<byte> destination, out int bytesConsumed, out int bytesWritten, bool isFinalBlock)
-        //{
+        public int Compress(ReadOnlySpan<byte> source, Span<byte> destination, int acceleration = 1)
+        {
+            ThrowIfDisposed();
 
-        //    fixed (byte* src = &MemoryMarshal.GetReference(source))
-        //    fixed (byte* dest = &MemoryMarshal.GetReference(destination))
-        //    {
-        //        LZ4_compress_fast_continue(stream, src, dest, source.Length, destination.Length, acceleration: 1);
+            fixed (byte* src = &MemoryMarshal.GetReference(source))
+            fixed (byte* dest = &MemoryMarshal.GetReference(destination))
+            {
+                var size = LZ4_compress_fast_continue(stream, src, dest, source.Length, destination.Length, acceleration: acceleration);
+                if (size < 0)
+                {
+                    Throw();
+                }
+                return size;
+            }
+        }
 
-
-        //        //var size = LZ4_decompress_safe_partial(src, dest, source.Length, targetOutputSize, destination.Length);
-        //        //if (size < 0)
-        //        //{
-        //        //    Throw(); // error.
-        //        //}
-        //        //return size;
-        //    }
-
-        //}
-
-
-        // public OperationStatus Compress(ReadOnlySpan<byte> source, Span<byte> destination, out int bytesConsumed, out int bytesWritten, bool isFinalBlock)
-        // public OperationStatus Flush(Span<byte> destination, out int bytesWritten)
+        void ThrowIfDisposed()
+        {
+            if (disposed)
+            {
+                throw new ObjectDisposedException("");
+            }
+        }
 
         public void Dispose()
         {
             if (stream != null)
             {
-                var foo = LZ4_freeStream(stream); // return value???
+                LZ4_freeStream(stream);
                 stream = null;
+                disposed = true;
             }
         }
     }
---
+++ tests/NativeCompressions.Tests/LZ4Test.cs
@@ -0,0 +1,42 @@
+﻿using NativeCompressions.Lz4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NativeCompressions.Tests;
+
+public class LZ4Test
+{
+    [Fact]
+    public void EncoderContinueCompress()
+    {
+        var src = new byte[1024];
+        var totalWrite = 0;
+        var span = src.AsSpan();
+        using var encoder = new LZ4Encoder();
+
+        var written = encoder.Compress(EncodeUtf8("あいうえおあいうえおあいうえお"), span);
+        totalWrite += written;
+        span = span.Slice(written);
+
+        written = encoder.Compress(EncodeUtf8("abcdefghijklmnopqrstu"), span);
+        totalWrite += written;
+        span = span.Slice(written);
+
+        var compressed = src.AsSpan(0, totalWrite).ToArray();
+
+        var dest = new byte[1024];
+        var ok = LZ4Encoder.TryDecompress(compressed, dest, out written);
+        ok.Should().BeTrue();
+        var str = Encoding.UTF8.GetString(dest.AsSpan(0, written));
+
+        str.Should().Be("あいうえおあいうえおあいうえおabcdefghijklmnopqrstu");
+    }
+
+    byte[] EncodeUtf8(string value)
+    {
+        return Encoding.UTF8.GetBytes(value);
+    }
+}
