e5f56554fb7783cc7760e4ef58d6ea334e8b438c
spacekey <fumihiro.fukuda@gmail.com>
2023-07-28 16:58:50 +0800

    Add Lz4CompressionProvider test

--- tests/NativeCompressions.Lz4.AspNetCoreCompressionProvider.Tests/Lz4CompressionProviderTest.cs
+++ tests/NativeCompressions.Lz4.AspNetCoreCompressionProvider.Tests/Lz4CompressionProviderTest.cs
@@ -1,3 +1,6 @@
+using System.IO.Compression;
+using System.Text;
+
 namespace NativeCompressions.Lz4.AspNetCoreCompressionProvider.Tests;
 
 public class Lz4CompressionProviderTest
@@ -5,11 +8,19 @@ public class Lz4CompressionProviderTest
     [Fact]
     public void BasicTest()
     {
-        var span = "Lz4CompressionProvider test"u8;
+        // Encode
         var provider = new Lz4CompressionProvider(new Lz4CompressionProviderOptions());
+        using var writeStream = new MemoryStream();
+        using var compressionStream = provider.CreateStream(writeStream);
+        compressionStream.Write("Lz4CompressionProvider test"u8);
+        compressionStream.Flush();
+        writeStream.Position = 0;
 
-        using var memoryStream = new MemoryStream();
-        using var compressionStream = provider.CreateStream(memoryStream);
-        compressionStream.Write(span);
+        // Decode
+        var lz4Stream = new LZ4Stream(writeStream, CompressionMode.Decompress);
+        var dest = new byte[1024];
+        var length = lz4Stream.Read(dest);
+        var str = Encoding.UTF8.GetString(dest[..length]);
+        Assert.Equal("Lz4CompressionProvider test", str);
     }
 }
