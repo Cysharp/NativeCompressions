f5ee2324140e86125a058ba45d47073b86dcdb31
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2026-09-17 04:51:52 +0900

    chore: remove gomi

--- sandbox/SmokeCatalyst/Middle/Compression.cs
+++
@@ -1,8 +0,0 @@
-namespace CatalystSmoke.Middle;
-
-public static unsafe class Compression
-{
-    public static uint OpenZLEncodingVersion => NativeCompressions.Interop.OpenZLNativeMethods.ZL_getDefaultEncodingVersion();
-    public static int VersionNumber => NativeCompressions.LZ4.VersionNumber;
-    public static uint ZstandardVersionNumber => NativeCompressions.Zstandard.VersionNumber;
-}
